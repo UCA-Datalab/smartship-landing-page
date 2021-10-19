d52d69a4c7a5e167f54052c01e46ef995187e86d
Javier-Jimenez99 <javijj99@gmail.com>
2021-10-19 13:38:36 +0200

    add api usage

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("results.html")
+    response = requests.get("http://127.0.0.1:5000/api/currents/11-12-2020")
+    currents = response.json()["currents"]
+
+    return render_template("results.html", currents=currents)
 
 
 if __name__ == "__main__":
