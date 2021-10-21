735b5b6bdc776f76855da8154b21d9e11d8be85b
Javier-Jimenez99 <javijj99@gmail.com>
2021-10-21 12:47:16 +0200

    first entry point added

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request
+import requests
+from requests.api import options
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("results.html")
+    response = requests.get("http://localhost:5000/api/available_routes/dummy")
+    code = response.status_code
+    if code == 200:
+        city_options = {
+            f"{cities['start_city']}-{cities['start_city']}": [
+                cities["start_city"],
+                cities["start_city"],
+            ]
+            for cities in response.json()
+        }
+    else:
+        city_options = {
+            "CADIZ-BOSTON": ["CADIZ", "BOSTON"],
+            "BOSTON-CADIZ": ["BOSTON", "CADIZ"],
+            "CARACAS-DAKAR": ["CARACAS", "DAKAR"],
+            "DAKAR-CARACAS": ["DAKAR", "CARACAS"],
+        }
 
-@app.route("/form_example")
-def form():
-    return render_template("form_example.html")
+    return render_template("form.html", city_options=city_options)
+
+
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    return render_template("results.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
