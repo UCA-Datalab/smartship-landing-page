66015ff919685f40edad78c4b4386de55af40c84
Javier-Jimenez99 <javijj99@gmail.com>
2021-10-25 12:56:22 +0200

    add control layer

--- static/libs/currents.js
+++ static/libs/currents.js
@@ -1,4 +1,6 @@
 function initDemoMap() {
+  var map = L.map("map");
+
   background_map = L.tileLayer('https://tile.jawg.io/9e47bb8d-efe9-44b4-86c8-e1ca9acbea38/{z}/{x}/{y}{r}.png?access-token=I6EpM0rUPAVxyVtfSFHyZJ6besx7JYPVnVr060qbSzw3g90ZfxhY09cwQYGlRC3f', {});
   /*
   var Esri_DarkGreyCanvas = L.tileLayer(
@@ -21,38 +23,49 @@ function initDemoMap() {
 }
 
 function generateMap(currents, base, best_route, city_start, city_end) {
-  // demo map
-  var map = initDemoMap();
+  var map = L.map("map");
 
-  // load data (u, v grids) from somewhere (e.g. https://github.com/danwild/wind-js-server)
+  var background_map = L.tileLayer('https://tile.jawg.io/9e47bb8d-efe9-44b4-86c8-e1ca9acbea38/{z}/{x}/{y}{r}.png?access-token=I6EpM0rUPAVxyVtfSFHyZJ6besx7JYPVnVr060qbSzw3g90ZfxhY09cwQYGlRC3f', {}).addTo(map);
 
 
-  var velocityLayer = L.velocityLayer({
+  var currents_layer = L.velocityLayer({
     displayValues: true,
     displayOptions: {
       velocityType: "Global Currents",
       position: "bottomleft",
       emptyString: "No currents data"
     },
+    lineWidth: 2,
     data: currents,
     maxVelocity: Math.max(Math.max(currents[0].data), Math.max(currents[1].data)),
     minVelocity: Math.min(Math.min(currents[0].data), Math.min(currents[1].data)),
   });
 
-  velocityLayer.addTo(map)
 
-  L.polyline(best_route, { color: 'red', weight: 1, opacity: 0.8 }).addTo(map);
-  L.polyline(base, { color: 'green', weight: 1, opacity: 1 }).addTo(map);
+  var best_r = L.polyline(best_route, { color: 'red', weight: 1.5, opacity: 0.8 });
+  var base_r = L.polyline(base, { color: 'green', weight: 1.5, opacity: 1 });
 
   var coord_start = base[0]
   var coord_end = base[base.length - 1]
 
-  var init_marker = L.marker(coord_start).addTo(map)
-  var end_marker = L.marker(coord_end).addTo(map)
+  var init_marker = L.marker(coord_start)
+  var end_marker = L.marker(coord_end)
 
-  init_marker.bindPopup("<b>" + city_start + "</b>").openPopup()
-  end_marker.bindPopup("<b>" + city_end + "</b>").openPopup()
+  init_marker.bindPopup("<b>" + city_start + "</b>", { closeOnClick: false, autoClose: false })
+  end_marker.bindPopup("<b>" + city_end + "</b>", { closeOnClick: false, autoClose: false })
+
+  var routes_group = L.layerGroup([best_r, base_r, init_marker, end_marker])
+
+  var overlay_layers = {
+    "Currents": currents_layer.addTo(map),
+    "Routes": routes_group.addTo(map),
+  }
+
+  L.control.layers({}, overlay_layers).addTo(map);
 
   map.setView([(coord_start[0] + coord_end[0]) / 2, (coord_start[1] + coord_end[1]) / 2], 4);
+
+  init_marker.openPopup()
+  end_marker.openPopup()
 }
 
