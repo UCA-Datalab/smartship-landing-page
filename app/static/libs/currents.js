94aab2306c5d42339ecb2ec8046019e59a3c5665
Javier <javijj99@gmail.com>
2021-10-31 13:16:56 +0100

    black formater

--- app/static/libs/currents.js
+++ app/static/libs/currents.js
@@ -82,13 +82,13 @@ function generateMap(waves, wind, currents, base, best_route, city_start, city_e
   })
 
   //HEATMAP
-  var heat = L.heatLayer(waves.height, { radius: 30, maxZoom:8, scaleRadius:false });
+  var heat = L.heatLayer(waves.height, { radius: 30, maxZoom: 8, scaleRadius: false });
   var waves_group = L.layerGroup([heat, waveLayer])
 
 
   //Potting routes and markers
-  var best_r = L.polyline.antPath(best_route, { color: '#00ab41', weight: 1.7, opacity: 0.8, delay: 500, dashArray: [3,40]  });
-  var base_r = L.polyline.antPath(base, { color: '#cc4902', weight: 1.7, opacity: 1, delay: 500, dashArray: [2,40] });
+  var best_r = L.polyline.antPath(best_route, { color: '#00ab41', weight: 1.7, opacity: 0.8, delay: 500, dashArray: [3, 40] });
+  var base_r = L.polyline.antPath(base, { color: '#cc4902', weight: 1.7, opacity: 1, delay: 500, dashArray: [2, 40] });
 
   var coord_start = base[0]
   var coord_end = base[base.length - 1]
