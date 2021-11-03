6df6d14f7754e3bc8284038d5097bb292bb21e8b
Javier-Jimenez99 <javijj99@gmail.com>
2021-11-03 19:37:37 +0100

    create function

--- app/static/libs/charts.js
+++ app/static/libs/charts.js
@@ -1,45 +1,47 @@
-new Chart(document.getElementById("line-chart"), {
-    type: 'line',
-    data: {
-        labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
-        datasets: [{
-            data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
-            label: "Base route",
-            borderColor: "#3e95cd",
-            fill: false
-        }, {
-            data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
-            label: "Optimize route",
-            borderColor: "#8e5ea2",
-            fill: false
-        }
-        ]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Fuel consumption between waypoints'
-        }
-    }
-});
-
-new Chart(document.getElementById("doughnut-chart"), {
-    type: 'doughnut',
-    data: {
-        labels: ["Spain", "United States", "Portugal", "France"],
-        datasets: [
-            {
-                label: "Miles",
-                backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
-                data: [2478, 5267, 734, 784]
+function generateCharts(cumulative_best_fuel, cumulative_base_fuel) {
+    new Chart(document.getElementById("line-chart"), {
+        type: 'line',
+        data: {
+            labels: cumulative_base_fuel.x,
+            datasets: [{
+                data: cumulative_base_fuel.y,
+                label: "Base route",
+                borderColor: "#3e95cd",
+                fill: false
+            }, {
+                data: cumulative_best_fuel.y,
+                label: "Optimize route",
+                borderColor: "#8e5ea2",
+                fill: false
             }
-        ]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Miles in territorial waters'
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Fuel consumption between waypoints'
+            }
         }
-    }
-});
+    });
 
+    new Chart(document.getElementById("doughnut-chart"), {
+        type: 'doughnut',
+        data: {
+            labels: ["Spain", "United States", "Portugal", "France"],
+            datasets: [
+                {
+                    label: "Miles",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9"],
+                    data: [2478, 5267, 734, 784]
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Miles in territorial waters'
+            }
+        }
+    });
+
+}
