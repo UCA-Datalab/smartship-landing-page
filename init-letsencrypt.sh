d0c1f253a4b891609ef8312830e2d689c0465651
franamor98 <franamorroldan@gmail.com>
2021-12-02 09:00:22 +0100

    cambiar certicicados a /rtc/ssl/

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,8 +7,8 @@ fi
 
 domains=(smartshipping.es www.smartshipping.es)
 rsa_key_size=4096
-data_path="./data/certbot"
-email="franamorroldan@gmail.com" # Adding a valid address is strongly recommended
+data_path="/etc/ssl/certbot_smartshipping"
+email="choped123@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
