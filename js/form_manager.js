7af5d793dd24d214eeae8fa316ce7a03c7f0846f
franamor98 <franamorroldan@gmail.com>
2022-07-25 10:53:06 +0200

    form manager

--- js/form_manager.js
+++ js/form_manager.js
@@ -1,10 +1,15 @@
 document.addEventListener("submit", (e) => {
+
+    form = e.target
+   
     $.ajax({
         url:'https://api.apispreadsheets.com/data/lxUiJTm9auzfiPz9/',
         type:'post',
         data:$("#FormDemo").serializeArray(),
         success: function(){
           console.log("Form Data Submitted :)")
+          alert("Thanks for your interest!");
+          form.reset();
         },
         error: function(){
           console.log("There was an error :(")
